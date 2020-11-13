a23eada1097f61d6007b5d9af6ca3df6ae46f27f
johnshields <G00348436@gmit.ie>
2020-11-13 15:27:47 +0000

    Basic player movement

---
+++ theGAME/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hMove = Input.GetAxis("Horizontal") * moveSpeed / 2;
+        float vMove = Input.GetAxis("Vertical") * moveSpeed;
+
+        transform.Translate(new Vector3(hMove, 0, vMove) * Time.deltaTime);
+    }
+}
---
+++ theGAME/Assets/Scripts/camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private Transform player;
+
+    private float yOffset = 12f;
+    private float zOffset = -5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform; 
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y + yOffset,player.position.z + zOffset);
+    }
+}
