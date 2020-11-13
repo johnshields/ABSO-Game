099fdb4ac9a6f7cf94f2a8115b400a5b52ee0245
johnshields <G00348436@gmit.ie>
2020-11-13 17:26:34 +0000

    player movement
    player can now run, jump and move side to side.

--- theGAME/Assets/Scripts/Player.cs
+++ theGAME/Assets/Scripts/Player.cs
@@ -4,20 +4,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 25f;
+    [SerializeField] public float moveSpeed = 25f;
+    [SerializeField] public float jumpHeight = 20f;
+    [SerializeField] public float Feet = 20f;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Run();
+        Jump();
+    }
+
+    private void Run()
+    {
+        // walk foward + move side to side
         float hMove = Input.GetAxis("Horizontal") * moveSpeed / 2;
         float vMove = Input.GetAxis("Vertical") * moveSpeed;
 
         transform.Translate(new Vector3(hMove, 0, vMove) * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if (transform.position.y <=Feet) {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight);
+                
+            }
+        }
     }
 }
