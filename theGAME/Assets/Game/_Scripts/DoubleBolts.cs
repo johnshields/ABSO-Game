036fea0a6e128fbfb5b6efb4bfd57c8c09e5dcf7
johnshields <G00348436@gmit.ie>
2020-12-04 00:16:56 +0000

    Level 3 complete +Power Ups + New Controls
    Level 3 is now complete. I have added in the power ups Double Bolts and Invincibility  to all levels (Level 1 has no invincibility). I also discovered you can change the controls in the Input Manager. So i've set up: 1234 and tfgh for character controls. This will suffice for my attempt at the key bind script... Now all I have to do is fix some audio elements in the game and add the multiplayer functionality...

---
+++ theGAME/Assets/Game/_Scripts/DoubleBolts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoubleBolts : MonoBehaviour
+{
+    [SerializeField] AudioClip doubleBoltsSound;
+
+    // rotate bolt
+    void Update()
+    {
+        transform.Rotate(new Vector3(1f, 5f, 5f));
+    }
+
+    // play pickup sound, add score and delete bolt from game
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioSource.PlayClipAtPoint(doubleBoltsSound, Camera.main.transform.position);
+        Destroy(gameObject);
+
+        // add to the bolt counter
+        other.GetComponent<BoltCounter>().bolts+=2;
+    }
+}
+
---
+++ theGAME/Assets/Game/_Scripts/PowerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUp : MonoBehaviour
+{
+    [SerializeField] AudioClip powerUpSound;
+
+    // rotate power up
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 1.5f, 0f));
+    }
+
+    // play power up sound, add score and delete power up from game
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioSource.PlayClipAtPoint(powerUpSound, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
+
